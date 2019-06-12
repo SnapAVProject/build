5b377268c47395b770de3e9c34cd6a7d3d4d95e8
kean <kean.wu@hansong-china.com>
2019-06-12 15:26:23 +0800

    add boardtype select

--- upload.sh
+++ upload.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+boardtype=$2
+
+if [ $boardtype = 'snapav2d' ];then
 sshpass -p wjh sftp cean@10.10.6.91 <<!
-
 put $1 /usr/share/nginx/html/snapav/
-
 !
+elif [ $boardtype = 'snapav8d' ];then
+sshpass -p wjh sftp cean@10.10.6.91 <<!
+put $1 /usr/share/nginx/html/snapav8d/
+!
+
+fi
+
