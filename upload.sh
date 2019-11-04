93d5cda2c173e102d9f4b63c58ebdf7a1ee32488
kean <kean.wu@hansong-china.com>
2019-11-04 16:39:24 +0800

    update 5.1 kernel config

--- upload.sh
+++ upload.sh
@@ -3,9 +3,9 @@ boardtype=$2
 echo $boardtype
 if [ $boardtype = 'snapav2d' ];then
 cp $1 fw/snapav/snapav-firmware/boot.img
-#sshpass -p wjh sftp cean@10.10.6.91 <<!
-#put $1 /usr/share/nginx/html/snapav/
-#!
+sshpass -p wjh sftp cean@10.10.6.91 <<!
+put $1 /usr/share/nginx/html/snapav/
+!
 elif [ $boardtype = 'snapav8d' ];then
 cp $1 fw/snapav8d/snapav-firmware/boot.img
 sshpass -p wjh sftp cean@10.10.6.91 <<!
