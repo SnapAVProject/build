7838edbf366ae1c448cc82869d794cffe55db52d
Steve Yang <steve.yang@hansong-china.com>
2020-01-07 10:07:56 +0800

    try fix the break build
    Change-Id: If30a43cc179006ae0c53a4c7d3792f1c34b62b80

--- root.mk
+++ root.mk
@@ -32,9 +32,9 @@ endif
 # By default, build the whole system
 all: $(TARGET)
 ifeq ($(BOARDTYPE), spa25)
-$(TARGET): uboot rootfs kernel app tools dirac
+$(TARGET): uboot rootfs kernel app tools web dirac
 else
-$(TARGET): uboot rootfs kernel app tools
+$(TARGET): uboot rootfs kernel app tools web
 endif
 
 .PHONY:app
@@ -71,11 +71,7 @@ tools:
 
 ###### Rootfs ######
 .PHONY:rootfs
-ifeq ($(BOARDTYPE), spa25)
-rootfs: tools dirac
-else
-rootfs: tools
-endif
+rootfs:
 	@echo '====== Building the Rootfs ======'
 #@ln -sf $(PWD)/${TOOLS_SRC} $(HANSONGTOOLS)/package
 	@cp build/buildrootconfig/${BOARDTYPE}_defconfig ${ROOTFS_SRC}/configs/${BOARDTYPE}_defconfig
@@ -84,6 +80,17 @@ endif
 	@cp $(PWD)/prebuilt/arm9-nuvoton/usr/arm-buildroot-linux-gnueabi/lib/libstdc++.so.6.0.24 ${PWD}/${OUTDIR}/target/lib/
 	cd ${OUTDIR}/target/lib/ && ln -sf libstdc++.so.6.0.24 libstdc++.so.6 && ln -sf libstdc++.so.6.0.24 libstdc++.so
 #	@cp ${OUTDIR}/images/rootfs.squashfs image/rootfs.img
+
+
+###### web pages ######
+
+.PHONY:web
+web:
+	cp -rf ${PWD}/web/src/*  ${PWD}/${OUTDIR}/target/usr/html
+	install -D --mode=0755 ${PWD}/web/S50nginx  ${PWD}/${OUTDIR}/target/etc/init.d/
+	install -D --mode=0644 ${PWD}/web/nginx.conf  ${PWD}/${OUTDIR}/target/etc/nginx/
+
+
 
 ###### Dirac ######
 .PHONY:dirac
@@ -94,7 +101,7 @@ dirac:
 
 ###### SW Release ######
 .PHONY:release
-release:
+release: rootfs
 	@echo '====== Releasing ======'
 	@echo $(PWD)
 	bash build/package.sh release
