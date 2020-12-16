6dd84440ef559712a805cd610344abb49a8a9ff9
kean.wu <kean.wu@hansonggroup.com>
2020-12-16 17:06:45 +0800

    28787 - update build.sh to fix web dir not correct issue
    Change-Id: Icef350a7fdde6e425e6d3a7f4bdd9a2ef2239f84

--- build.sh
+++ build.sh
@@ -91,7 +91,6 @@ buildkernel(){
 buildrootfs(){
 	echo buildrootfs
 	echo "============ checking out web update ================"
-	git submodule update --init --recursive
 	echo "============ web checking finish     ================"
 	if [ $boardtype = 'snapav2d'  ];then
 	 if [ -d ../web/snapav_2d_web ];then 
@@ -105,23 +104,24 @@ buildrootfs(){
 	fi
 	elif [ $boardtype = 'snapav12d' ];then
 	 if [ -d ../web/snapav_12d_web ];then 
-	 	cp ../web/snapav12d	../nuc970_buildroot/board/nuvoton/hs_rootfs_12d/usr/html/  -a
+	 	cp ../web/snapav_12d_web ../nuc970_buildroot/board/nuvoton/hs_rootfs_12d/usr/html/  -a
 		 rm ../nuc970_buildroot/board/nuvoton/hs_rootfs_12d/usr/html/snapav_12d_web/.git	
  	 fi
 	elif [ $boardtype = 'snapav16d' ];then
 	 if [ -d ../web/snapav_16d_web ];then 
-	  	cp ../web/snapav16d  ../nuc970_buildroot/board/nuvoton/hs_rootfs_16d/usr/html/  -a
+	  	cp ../web/snapav_16d_web  ../nuc970_buildroot/board/nuvoton/hs_rootfs_16d/usr/html/  -a
 		 rm ../nuc970_buildroot/board/nuvoton/hs_rootfs_16d/usr/html/snapav_16d_web/.git	
      fi
 	elif [ $boardtype = 'snapav51' ];then
 	 if [ -d ../web/snapav_5.1_web ];then 
-	  	cp ../web/snapav5.1  ../nuc970_buildroot/board/nuvoton/hs_rootfs_51/usr/html/  -a
+	  	cp ../web/snapav_5.1_web  ../nuc970_buildroot/board/nuvoton/hs_rootfs_51/usr/html/  -a
 		 rm ../nuc970_buildroot/board/nuvoton/hs_rootfs_51/usr/html/snapav_5.1_web/.git	
  	 fi
 	else
 		echo "unsupport  board $boardtype"
 	fi
 
+	echo "============ web checking finish     ================"
 	cd ../nuc970_buildroot/
 
 	echo cp ../build/buildrootconfig/${boardtype}_buildroot_config .config
